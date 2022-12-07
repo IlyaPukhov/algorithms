03f279d87b185b3fcffa56f7f146710d39b1962a
IlyaPukhov <dinamond2003@gmail.com>
2022-12-08 02:26:04 +0300

    Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,95 +1,30 @@
+/*
+   (1) элемент Tree(x), где x: T - бинарное дерево,
+   (2) пустой элемент null - бинарное дерево
+   (3) A и B - бинарные деревья, а x - элемент типа T,
+       тогда (x, A, B) - бинарное дерево,
+       A - левым потомок
+       B - правый потомок
+
+       x
+    A     B
+
+    Бинарное дерево поиска - это бинарное дерево со сравнимыми элементами
+    с доп. условием:
+    все элементы в левом поддереве меньше корня
+    все элементы в правом поддереве больше или равны корня.
+*/
 fun main() {
-    /*
-    a = [1, 2, 5, 4, 6, 8, 7]
-    x = 4
-    indexOf(x)
-    O(n) - поиск элемента в произвольном массиве.
-    Бинарный поиск
-    a = [1, 4, 5, 8, 9, 18, 32, 68, 99]
-    x = 68
-    O(log n) - бинарный поиск в упорядоченном массиве
-
-    индексация
-    */
-    val array = arrayListOf(1, 1, 1, 5, 15, 17, 19, 22, 24, 31, 31, 31, 105, 150, 150)
-    val x = 150
-    val start = array.startIndex(x)
-    val end = array.endIndex(x)
-    println("Диапазон индексов элемента $x: $start..$end")
-    /*
-    Задача. Найти диапазон индексов, который заполняет данный элемент x
-    в упорядоченном массиве, за логарифмическое время.
-    */
-}
-
-fun <T : Comparable<T>> ArrayList<T>.endIndex(
-    value: T,
-    range: IntRange = indices
-): Int? {
-    if (range.first > range.last) {
-        return null
+    val tree = BinarySearchTree<Int>()
+    val list = listOf(5, 3, 2, 3, 1, 7, 6)
+    for (x in list) {
+        tree.insert(x)
     }
-    val size = range.last - range.first + 1
-    val middle = range.first + size / 2
-    if (value == this[middle]) {
-        if (middle == range.last) {
-            return range.last
-        }
-        if (this[middle + 1] != value) {
-            return middle
-        } else {
-            endIndex(value, middle + 1..range.last)
-        }
-    }
-    return if (value < this[middle]) {
-        endIndex(value, range.first until middle)
-    } else {
-        endIndex(value, middle + 1..range.last)
-    }
-}
-
-
-fun <T : Comparable<T>> ArrayList<T>.startIndex(
-    value: T,
-    range: IntRange = indices
-): Int? {
-    if (range.first > range.last) {
-        return null
-    }
-    val size = range.last - range.first + 1
-    val middle = range.first + size / 2
-    if (value == this[middle]) {
-        if (middle == range.first) {
-            return middle
-        }
-        return if (value == this[middle - 1]) {
-            startIndex(value, range.first until middle)
-        } else {
-            middle
-        }
-    }
-    return if (value < this[middle]) {
-        startIndex(value, range.first until middle)
-    } else {
-        startIndex(value, middle + 1..range.last)
-    }
-}
-
-fun <T : Comparable<T>> ArrayList<T>.binarySearch1(
-    value: T,
-    range: IntRange = indices
-): Int? {
-    if (range.first > range.last) {
-        return null
-    }
-    val size = range.last - range.first + 1
-    val middle = range.first + size / 2
-    if (value == this[middle]) {
-        return middle
-    }
-    return if (value < this[middle]) {
-        binarySearch1(value, range.first until middle)
-    } else {
-        binarySearch1(value, middle + 1..range.last)
-    }
+    println(tree)
+    tree.remove(5)
+    println(tree)
+    println(tree.find(1))
+    println(tree.find(5))
+    println(tree.find(1))
+    println(tree.nextNode(1))
 }
