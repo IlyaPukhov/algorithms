9fc0080e307dbc9006b3be37aa09b5140ca9dd6c
IlyaPukhov <dinamond2003@gmail.com>
2022-12-08 02:21:39 +0300

    Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,95 @@
 fun main() {
-    println(
-        "Добро пожаловать в Калькулятор \"Infix to postfix\"!\n" +
-                "Он может преобразовывать инфиксные выражения в постфиксные, \n" +
-                "а также вычислять значение постфиксного и инфиксного выражений\n" +
-                "Обладает довольно скудным функционалом и может предложить только следующие операции:\n" +
-                "Сложение(+), Вычитание(-), Умножение(*), Деление(/), Возведение в степень(^), Нахождение синуса (sin)"
-    )
+    /*
+    a = [1, 2, 5, 4, 6, 8, 7]
+    x = 4
+    indexOf(x)
+    O(n) - поиск элемента в произвольном массиве.
+    Бинарный поиск
+    a = [1, 4, 5, 8, 9, 18, 32, 68, 99]
+    x = 68
+    O(log n) - бинарный поиск в упорядоченном массиве
 
-    print("Входное выражение: ")
-    val stringExpr: String = readlnOrNull().toString()
+    индексация
+    */
+    val array = arrayListOf(1, 1, 1, 5, 15, 17, 19, 22, 24, 31, 31, 31, 105, 150, 150)
+    val x = 150
+    val start = array.startIndex(x)
+    val end = array.endIndex(x)
+    println("Диапазон индексов элемента $x: $start..$end")
+    /*
+    Задача. Найти диапазон индексов, который заполняет данный элемент x
+    в упорядоченном массиве, за логарифмическое время.
+    */
+}
 
-    print(
-        "Что вы хотите сделать с данным выражением?\n" +
-                "1) Преобразовать инфиксное выражение в постфиксное\n" +
-                "2) Вычислить значение постфиксного выражения\n" +
-                "3) Вычислить значение инфиксного выражения\n" +
-                "Введите номер нужной вам операции: "
-    )
-
-    val mode: String = readlnOrNull().toString()
-    when (mode.toInt()) {
-        1 ->
-            println("Постфиксная форма записи заданного выражения: ${Operations().infixToPostfix(stringExpr)}")
-
-        2 ->
-            println("Значение постфиксного выражения: ${Operations().calcPostfixExpr(stringExpr)}")
-
-        3 ->
-            println("Результат: ${Operations().calcInfixExpr(stringExpr)}")
-
-        else ->
-            throw Exception("Ошибка! Попробуйте ещё раз.")
+fun <T : Comparable<T>> ArrayList<T>.endIndex(
+    value: T,
+    range: IntRange = indices
+): Int? {
+    if (range.first > range.last) {
+        return null
+    }
+    val size = range.last - range.first + 1
+    val middle = range.first + size / 2
+    if (value == this[middle]) {
+        if (middle == range.last) {
+            return range.last
+        }
+        if (this[middle + 1] != value) {
+            return middle
+        } else {
+            endIndex(value, middle + 1..range.last)
+        }
+    }
+    return if (value < this[middle]) {
+        endIndex(value, range.first until middle)
+    } else {
+        endIndex(value, middle + 1..range.last)
     }
 }
 
-/**
-Expression: -123.7 + (12.1 ^ 6 - 14) * 0.16 / (18.3 + 5.9) + sin 8 ^ 2
-Result: -123.7 12.1 6 ^ 14 - 0.16 * 18.3 5.9 + / + 8 sin 2 ^ +
 
-Expression: (1 + 2) * 912.76 - 3 / (4 ^ 5 + 6) + sin 7 ^ 2
-Result: 1 2 + 912.76 * 3 4 5 ^ 6 + / - 7 sin 2 ^ +
- **/
+fun <T : Comparable<T>> ArrayList<T>.startIndex(
+    value: T,
+    range: IntRange = indices
+): Int? {
+    if (range.first > range.last) {
+        return null
+    }
+    val size = range.last - range.first + 1
+    val middle = range.first + size / 2
+    if (value == this[middle]) {
+        if (middle == range.first) {
+            return middle
+        }
+        return if (value == this[middle - 1]) {
+            startIndex(value, range.first until middle)
+        } else {
+            middle
+        }
+    }
+    return if (value < this[middle]) {
+        startIndex(value, range.first until middle)
+    } else {
+        startIndex(value, middle + 1..range.last)
+    }
+}
+
+fun <T : Comparable<T>> ArrayList<T>.binarySearch1(
+    value: T,
+    range: IntRange = indices
+): Int? {
+    if (range.first > range.last) {
+        return null
+    }
+    val size = range.last - range.first + 1
+    val middle = range.first + size / 2
+    if (value == this[middle]) {
+        return middle
+    }
+    return if (value < this[middle]) {
+        binarySearch1(value, range.first until middle)
+    } else {
+        binarySearch1(value, middle + 1..range.last)
+    }
+}
