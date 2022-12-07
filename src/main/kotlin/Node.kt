9cb6e9b8b5019e80ef87f910a152bd0e10761605
IlyaPukhov <dinamond2003@gmail.com>
2022-12-08 02:14:00 +0300

    Initial Commit

--- src/main/kotlin/Node.kt
+++ src/main/kotlin/Node.kt
@@ -1,10 +1,11 @@
 data class Node<T>(
     var value: T,
-    var next: Node<T>? = null
+    var next: Node<T>? = null,
+    var prev: Node<T>? = null
 ) {
     override fun toString(): String {
         return if (next != null) {
-            "$value -> ${next.toString()}"
+            "$value <-> ${next.toString()}"
         } else {
             "$value"
         }
